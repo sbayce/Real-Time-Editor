845c1c587488df24c89d78bc861642ba00806fc8
youssef khaled <youssef2311@gmail.com>
2024-06-16 19:29:45 +0300

    DB connection

---
+++ server/db.ts
@@ -0,0 +1,15 @@
+import pg from "pg"
+import dotenv from "dotenv"
+const { Pool } = pg
+
+dotenv.config()
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: 5432,
+})
+
+export default pool
