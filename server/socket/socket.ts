46b0b8ad160c53bf357214ce43eef5023ff65852
youssef khaled <youssef2311@gmail.com>
2024-06-20 02:02:59 +0300

    local memory socket.io

--- server/socket/socket.ts
+++ server/socket/socket.ts
@@ -3,6 +3,17 @@ import { Server } from "socket.io"
 import express from "express"
 import pool from "../db"
 
+type User = {
+  userId: string
+}
+type Socket = {
+  socketId: string
+}
+
+type RoomMap = {
+  [roomId: string]: [socketId: string]
+}
+
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
@@ -12,10 +23,37 @@ const io = new Server(server, {
   },
 })
 
+const roomMap = new Map<string, Map<string, string>>()
+const socketMap = new Map<string, string>()
+
 io.on("connection", (socket) => {
-  socket.on("join_room", (data) => {
-    console.log("joined room: " + data)
-    socket.join(data)
+  // console.log("auth: " + JSON.stringify(socket.handshake.auth.userId))
+  // const userId = socket.handshake.auth.userId
+  var roomNum: string
+  const socketId = socket.id
+  socket.on("join_room", (roomId, userId) => {
+    console.log("joined room: " + roomId)
+    roomNum = roomId
+    socketMap.set(socketId, userId)
+    roomMap.set(roomId, socketMap)
+
+    socket.join(roomId)
+    const roomMapObject = Array.from(roomMap.entries()).reduce(
+      (obj, [key, value]) => {
+        obj[key] = Object.fromEntries(value)
+        return obj
+      },
+      {} as any
+    )
+    console.log("map: " + JSON.stringify(roomMapObject))
+  })
+  socket.on("disconnect", () => {
+    console.log("socket id: " + socketId + " disconnected.")
+    console.log(roomNum)
+    if (roomNum) {
+      socketMap.delete(socketId)
+      roomMap.set(roomNum, socketMap)
+    }
   })
   // socket.on("send_message", async (data) => {
   //   console.log("send message: " + data.message + " to room: " + data.room)
