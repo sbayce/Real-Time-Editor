47bce2668a59a57368bcfa708972b092cf1ca6a8
youssef khaled <youssef2311@gmail.com>
2024-06-21 02:50:32 +0300

    socket.io backend

--- server/socket/socket.ts
+++ server/socket/socket.ts
@@ -1,18 +1,6 @@
 import http from "http"
 import { Server } from "socket.io"
 import express from "express"
-import pool from "../db"
-
-type User = {
-  userId: string
-}
-type Socket = {
-  socketId: string
-}
-
-type RoomMap = {
-  [roomId: string]: [socketId: string]
-}
 
 const app = express()
 const server = http.createServer(app)
@@ -26,45 +14,64 @@ const io = new Server(server, {
 const roomMap = new Map<string, Map<string, string>>()
 const socketMap = new Map<string, string>()
 
-io.on("connection", (socket) => {
-  // console.log("auth: " + JSON.stringify(socket.handshake.auth.userId))
-  // const userId = socket.handshake.auth.userId
-  console.log("new socket: " + socket.id)
-  var roomNum: string
-  const socketId = socket.id
-  socket.on("join_room", (roomId, userId) => {
-    console.log("socket: " + socketId + " joined room: " + roomId)
-    roomNum = roomId
-    socketMap.set(socketId, userId)
-    roomMap.set(roomId, socketMap)
-
-    socket.join(roomId)
-    const roomMapObject = Array.from(roomMap.entries()).reduce(
-      (obj, [key, value]) => {
-        obj[key] = Object.fromEntries(value)
-        return obj
-      },
-      {} as any
-    )
-    console.log("map: " + JSON.stringify(roomMapObject))
+function logRoomMap(roomMap: Map<string, Map<string, string>>): void {
+  roomMap.forEach((innerMap, room) => {
+    console.log(`Room: ${room}`)
+    innerMap.forEach((user, socket) => {
+      console.log(`  User: ${user}, Socket: ${socket}`)
+    })
   })
+}
+
+io.on("connection", (socket) => {
+  console.log("new socket: " + socket.id)
+  const roomId = socket.handshake.auth.roomId
+  const userEmail = socket.handshake.auth.userEmail
+  const socketId = socket.id
+  let currentRoomMap = roomMap.get(roomId)
+  if (!currentRoomMap) {
+    var newRoomMap = new Map<string, string>()
+    newRoomMap.set(socketId, userEmail)
+    roomMap.set(roomId, newRoomMap)
+  } else {
+    currentRoomMap?.set(socketId, userEmail)
+    roomMap.set(roomId, currentRoomMap)
+  }
+  // socketMap.set(socketId, userEmail)
+  // roomMap.set(roomId, socketMap)
+  socket.join(roomId)
+  console.log("socket: " + socketId + " joined room: " + roomId)
+  logRoomMap(roomMap)
+  const userMap = roomMap.get(roomId)
+  if (userMap) {
+    const userEmails = Array.from(userMap.values())
+    io.to(roomId).emit("online_users", userEmails)
+  }
+
   socket.on("disconnect", () => {
     console.log("socket id: " + socketId + " disconnected.")
-    console.log(roomNum)
-    if (roomNum) {
-      socketMap.delete(socketId)
-      roomMap.set(roomNum, socketMap)
+    console.log(roomId)
+    var currentRoomMap = roomMap.get(roomId)
+    if (currentRoomMap) {
+      currentRoomMap.delete(socketId)
+      if (currentRoomMap.size === 0) {
+        // delete room if its empty
+        roomMap.delete(roomId)
+      } else {
+        roomMap.set(roomId, currentRoomMap)
+      }
+    }
+    // socketMap.delete(socketId)
+    // roomMap.set(roomId, socketMap)
+    logRoomMap(roomMap)
+    const userMap = roomMap.get(roomId)
+    if (userMap) {
+      const userEmails = Array.from(userMap.values())
+      io.to(roomId).emit("online_users", userEmails)
     }
   })
-  // socket.on("send_message", async (data) => {
-  //   console.log("send message: " + data.message + " to room: " + data.room)
-  //   socket.to(data.room).emit("recieve_message", data.message)
-  //   await pool.query("UPDATE editor SET content = $1 WHERE id = $2", [
-  //     data.message,
-  //     data.room,
-  //   ])
-  // })
 })
+
 io.on("connection_error", (err) => {
   console.log(`connect_error due to ${err.message}`)
 })
