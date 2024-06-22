5a1f8d778ae6a33fdbfc4fdb76da3870a545bbd5
youssef khaled <youssef2311@gmail.com>
2024-06-22 03:47:15 +0300

    integrated quill editor

--- client/app/editor/[editorId]/page.tsx
+++ client/app/editor/[editorId]/page.tsx
@@ -1,19 +1,18 @@
 "use client"
-import React from "react"
+import React, { useCallback } from "react"
 import { usePathname } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { io, Socket } from "socket.io-client"
 import axios from "axios"
-import updateContent from "@/app/lib/editor/update-content"
 import { Chip } from "@nextui-org/react"
-// import socket from "@/app/lib/socket"
-
-// var socket: Socket = io("http://localhost:4000")
+import Quill from "quill"
+import "quill/dist/quill.snow.css"
+import toolbarOptions from "@/app/lib/editor/quil-toolbar"
 
 type Editor = {
   id: string
   title: string
-  content: string
+  content: any
   created_at: string
   updated_at: string
   userEmail: string
@@ -21,11 +20,12 @@ type Editor = {
 
 const page = () => {
   const [editorData, setEditorData] = useState<Editor | null>(null)
-  const [text, setText] = useState("")
   const [title, setTitle] = useState(editorData?.title)
   const [isEditableTitle, setIsEditableTitle] = useState(false)
   const [socket, setSocket] = useState<Socket | null>(null)
   const [onlineUsers, setOnlineUsers] = useState([])
+  const [quill, setQuill] = useState<Quill | null>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const viewEditor = async () => {
     try {
@@ -37,45 +37,61 @@ const page = () => {
       )
       const data = res.data
       setEditorData(data)
-      setText(data.content)
     } catch (error: any) {
       console.log(error.message)
     }
   }
-  const handleContentChange = async (event: any) => {
-    try {
-      const textValue = event.target.value
-      setText(textValue)
-      updateContent(textValue, editorId)
-    } catch (err) {
-      console.log(err)
-    }
+
+  const onTitleSubmit = (event: any) => {
+    if (!socket) return
+    event.preventDefault()
+    socket.emit("send_title", title)
+    setIsEditableTitle(false)
   }
   useEffect(() => {
     viewEditor()
   }, [])
 
   useEffect(() => {
-    if (!editorData) {
+    if (!editorData || !quill) {
       return
     }
-    if (editorData) {
-      const socket: Socket = io("http://localhost:4000", {
-        auth: {
-          roomId: editorId,
-          userEmail: editorData.userEmail,
-        },
-      })
-      setSocket(socket)
-      setTitle(editorData?.title)
-    }
-  }, [editorData])
+    const socket: Socket = io("http://localhost:4000", {
+      auth: {
+        roomId: editorId,
+        userEmail: editorData.userEmail,
+      },
+    })
+    setSocket(socket)
+    setTitle(editorData?.title)
+    quill.setContents(JSON.parse(editorData.content))
+    quill.enable()
+  }, [editorData, quill])
 
   useEffect(() => {
-    socket?.on("recieve_message", (data) => {
-      console.log("recieved message from controller: " + data)
-      setText(data)
+    if (!socket || !quill) return
+
+    socket.on("recieve-changes", (delta) => {
+      quill.updateContents(delta)
     })
+
+    socket.on("recieve_title", (title) => {
+      setTitle(title)
+    })
+
+    quill.on("text-change", (delta, oldDelta, source) => {
+      if (source !== "user") return
+      const content = quill.getContents()
+      socket.emit("send-changes", { delta: delta, content: content })
+    })
+
+    return () => {
+      quill.off("text-change")
+    }
+  }, [socket, quill])
+
+  useEffect(() => {
+    if (!socket) return
 
     socket?.on("online_users", (data) => {
       console.log("online users are: " + data)
@@ -88,7 +104,42 @@ const page = () => {
       }
     }
   }, [socket])
-  console.log(onlineUsers)
+
+  useEffect(() => {
+    if (isEditableTitle && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isEditableTitle])
+
+  const wrapperRef = useCallback((wrapper: any) => {
+    if (wrapper === null) return
+    wrapper.innerHTML = ""
+    const editor = document.createElement("div")
+    wrapper.append(editor)
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: toolbarOptions },
+    })
+    setQuill(q)
+  }, [])
+  const ql = document.querySelector(".ql-editor")
+  ql?.addEventListener("mouseup", () => {
+    let selection = window.getSelection()
+    console.log(selection)
+    if (selection && selection.rangeCount > 0) {
+      let range = selection.getRangeAt(0)
+      console.log("range: " + selection.toString())
+      let selectedText = range.toString()
+      console.log(selectedText)
+    }
+    // let parentElement = selection?.anchorNode?.parentElement
+    // parentElement?.addEventListener("mouseup", () => {
+    //   console.log(parentElement)
+    // })
+    // console.log(parentElement.select)
+  })
+
+  console.log(ql)
   const editorId = usePathname().split("/")[2]
   return (
     <div>
@@ -96,13 +147,11 @@ const page = () => {
         <>
           <div className="flex justify-between">
             <form
-              onSubmit={(e) => {
-                e.preventDefault()
-                setIsEditableTitle(false)
-              }}
+              onSubmit={onTitleSubmit}
               className="self-start flex justify-between"
             >
               <input
+                ref={inputRef}
                 value={title}
                 disabled={!isEditableTitle}
                 className="bg-transparent border-none hover:bg-none text-xl"
@@ -134,12 +183,7 @@ const page = () => {
             </div>
           </div>
           <div className="flex flex-col gap-4 items-center justify-center p-4">
-            <textarea
-              className="border rounded-lg p-2 w-full"
-              placeholder="Enter text"
-              onChange={handleContentChange}
-              value={text}
-            />
+            <div ref={wrapperRef}></div>
           </div>
         </>
       ) : (
