42a99023af45bea65dcfd4c2834ffbd88d4e6ba0
sbayce <youssef2311@gmail.com>
2024-06-29 14:21:31 +0300

    option to create editor

--- client/app/components/home/Workspace.tsx
+++ client/app/components/home/Workspace.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import { useState, useEffect } from "react"
 import { Image } from "@nextui-org/react"
 import { useRouter } from "next/navigation"
+import axios from "axios"
 
 type WorkspaceProps = {
   workspace: any
@@ -12,13 +13,19 @@ const Workspace = ({ workspace }: WorkspaceProps) => {
   const router = useRouter()
   const [data, setData] = useState(workspace)
   console.log(data)
+
   const dateFormat = (dateString: string) => {
     const date = new Date(dateString)
     return "Created " + date.toLocaleDateString()
   }
+  const createEditor = async() => {
+    const res = await axios.post("http://localhost:4000/user/create-editor", {}, {withCredentials: true})
+    setData([...data, res.data])
+  }
   return (
     <div className="grid grid-cols-4 gap-4">
-      {data.map((editor: any) => {
+      <button onClick={createEditor}>Create Editor</button>
+      {data && data.map((editor: any) => {
         return (
           <div
             key={editor.id}
