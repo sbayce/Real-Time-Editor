2523b9495b879dfc2f1c73c89017500f541792e5
sbayce <youssef2311@gmail.com>
2024-06-29 14:21:41 +0300

    connection to new db

--- server/db.ts
+++ server/db.ts
@@ -5,11 +5,15 @@ const { Pool } = pg
 dotenv.config()
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL
 })
+// console.log(pool.query('SELECT version()'))
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: 5432,
+// })
 
 export default pool
