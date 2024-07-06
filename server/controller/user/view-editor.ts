208f5b752244b6b477f0a8a47e030176a583bbc0
sbayce <youssef2311@gmail.com>
2024-07-06 04:17:04 +0300

    caching for editors

--- server/controller/user/view-editor.ts
+++ server/controller/user/view-editor.ts
@@ -1,22 +1,41 @@
 import { Request, Response } from "express"
 import checkAccessPermission from "../../lib/editor/check-access-permission"
+import redisClient from "../../redis"
+import { json } from "body-parser"
 
 const viewEditor = async (req: Request, res: Response) => {
   try {
     const { postgres } = req.context
     const userId = req.userId
     const editorId = req.params.editorId
-    const editorExists = await postgres.query(
-      "SELECT * FROM editor WHERE id = $1",
-      [editorId]
-    )
-    if (editorExists.rowCount === 0) {
-      res.status(400).json("Editor does not exist")
-      return
+    let editor
+    let editorContent
+    let restData
+    const cachedEditor: any = await redisClient.json.get(`editor:${editorId}`)
+    if(!cachedEditor){
+      const editorExists = await postgres.query(
+        "SELECT * FROM editor WHERE id = $1",
+        [editorId]
+      )
+      if (editorExists.rowCount === 0) {
+        res.status(400).json("Editor does not exist")
+        return
+      }
+      editor = editorExists.rows[0]
+      console.log(editor)
+      redisClient.json.set(`editor:${editorId}`, "$", editor)
+      redisClient.expire(`editor:${editorId}`, 300)
+    }else{
+      editor = cachedEditor
+      console.log("from cache")
+      const {content, ...rest} = cachedEditor
+      editorContent = content
+      restData = rest
     }
-    const editor = editorExists.rows[0]
+  
     const hasAccessPermission = checkAccessPermission(editor, userId)
     if (!hasAccessPermission) {
+      // redisClient.json.arrAppend(`user:${userId}`, "$", {[editorId]: false})
       res
         .status(403)
         .json({ message: "User is neither an owner nor a collaborator" })
@@ -26,8 +45,20 @@ const viewEditor = async (req: Request, res: Response) => {
       userId,
     ])
     const userEmail = user.rows[0].email
+    /* content is stored as JSON in redis and as STRING in posgres
+       So pass the content as STRING if it's coming from redis
+       The frontend accepts content as STRING
+    */
+    
+    if(editorContent){
+      let content = JSON.stringify(editorContent)
+      console.log("ok: ", content)
+      res.status(201).json({ content, ...restData, userEmail: userEmail })
+      return
+    }
     res.status(201).json({ ...editor, userEmail: userEmail })
   } catch (error) {
+    console.log(error)
     res.status(500).json(error)
   }
 }
