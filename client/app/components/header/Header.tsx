2dd8a72f239285cca183ecb6b355a7ec5e89e25b
sbayce <youssef2311@gmail.com>
2024-07-08 00:06:46 +0300

    fetch user data

--- client/app/components/header/Header.tsx
+++ client/app/components/header/Header.tsx
@@ -1,14 +1,23 @@
+"use client"
 import React from "react"
-import { User } from "@nextui-org/react"
+import { User, Avatar, Button } from "@nextui-org/react"
 import Link from "next/link"
+import { useQuery } from "react-query"
+import getUser from "@/app/lib/user/get-user"
 
 const Header = () => {
+  const {data, isLoading} = useQuery("me", getUser)
   return (
-    <header className="bg-gray-100 flex justify-between items-center p-2 border-b-1">
+    <header className="bg-gray-100 flex justify-between items-center py-2 px-10 border-b-1">
       <Link href="/home">Real-time Editor</Link>
-      <div className="flex flex-col">
-        <User name="sbace" description="sbace@gmail.com" />
-      </div>
+      {isLoading && <p className="text-sm">loading...</p>}
+      {data? <div className="flex flex-col">
+        <Avatar color="secondary" name={data.username} />
+        {/* <User name={data.username} description={data.email} /> */}
+      </div> :
+      (!isLoading && <Button>Login</Button>)
+      }
+      
     </header>
   )
 }
