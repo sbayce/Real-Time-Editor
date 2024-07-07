b62d8089f5a997041870f272cdd3a92731bd389d
sbayce <youssef2311@gmail.com>
2024-07-07 19:33:06 +0300

    return created editor details

--- server/controller/user/create-editor.ts
+++ server/controller/user/create-editor.ts
@@ -5,7 +5,7 @@ const createEditor = async (req: Request, res: Response) => {
     const { postgres } = req.context
     const userId = req.userId
     const editor = await postgres.query(
-      "INSERT INTO editor (owner) VALUES ($1) RETURNING *",
+      "INSERT INTO editor (owner) VALUES ($1) RETURNING id, title, created_at, updated_at",
       [userId]
     )
     const editorId = editor.rows[0].id
@@ -18,7 +18,7 @@ const createEditor = async (req: Request, res: Response) => {
       "UPDATE users SET workspace = workspace || $1::jsonb WHERE id = $2",
       [newWorkspaceEntry, userId]
     )
-    res.status(201).json({ message: "Editor created" })
+    res.status(201).json(editor.rows[0])
   } catch (error: any) {
     res.status(500).json(error.message)
   }
