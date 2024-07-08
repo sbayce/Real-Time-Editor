2c8777b5061b78f7c4b25aab9a6cb1212ba77a56
sbayce <youssef2311@gmail.com>
2024-07-08 05:07:10 +0300

    tailwind themes

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -18,6 +18,46 @@ const config: Config = {
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui({
+    themes: {
+      light: {
+        colors: {
+          background: "#FFFFFF", // or DEFAULT
+          foreground: "#11181C", // or 50 to 900 DEFAULT
+          primary: {
+            //... 50 to 900
+            foreground: "#FFFFFF",
+            DEFAULT: "#006FEE",
+          },
+          // ... rest of the colors
+        },
+      },
+      dark: {
+        colors: {
+          background: "#070a12",
+          background2: "#0d0d0d", // or DEFAULT
+          foreground: "#f4dbbc", // or 50 to 900 DEFAULT
+          foreground2: "#ECEDEE",
+          primary: {
+            //... 50 to 900
+            foreground: "#FFFFFF",
+            DEFAULT: "#006FEE",
+          },
+        },
+        // ... rest of the colors
+      },
+      mytheme: {
+        // custom theme
+        extend: "dark",
+        colors: {
+          primary: {
+            DEFAULT: "#BEF264",
+            foreground: "#000000",
+          },
+          focus: "#BEF264",
+        },
+      },
+    },
+  })],
 }
 export default config
