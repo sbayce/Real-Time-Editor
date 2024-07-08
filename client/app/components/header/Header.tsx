5671f0781231e3fd429b91f73b7723751fb03a27
sbayce <youssef2311@gmail.com>
2024-07-08 05:07:34 +0300

    profile dropdown

--- client/app/components/header/Header.tsx
+++ client/app/components/header/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react"
-import { User, Avatar, Button } from "@nextui-org/react"
+import { User, Avatar, Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem, cn } from "@nextui-org/react"
 import Link from "next/link"
 import { useQuery } from "react-query"
 import getUser from "@/app/lib/user/get-user"
@@ -8,11 +8,31 @@ import getUser from "@/app/lib/user/get-user"
 const Header = () => {
   const {data, isLoading} = useQuery("me", getUser)
   return (
-    <header className="bg-gray-100 flex justify-between items-center py-2 px-10 border-b-1">
+    <header className="flex justify-between items-center py-2 px-10">
       <Link href="/home">Real-time Editor</Link>
       {isLoading && <p className="text-sm">loading...</p>}
       {data? <div className="flex flex-col">
-        <Avatar color="secondary" name={data.username} />
+        <Dropdown>
+          <DropdownTrigger>
+            <Avatar color="secondary" name={data.username} className="cursor-pointer" />
+          </DropdownTrigger>
+          <DropdownMenu>
+            <DropdownSection>
+              <DropdownItem>
+              <div className="flex flex-col gap-2">
+                <p>{data.username}</p>
+                <p>{data.email}</p>
+              </div>
+              </DropdownItem>
+            </DropdownSection>
+            <DropdownSection>
+            <DropdownItem key="logout" className="text-danger" color="danger">
+              Logout
+            </DropdownItem>
+            </DropdownSection>
+          </DropdownMenu>
+        </Dropdown>
+        
         {/* <User name={data.username} description={data.email} /> */}
       </div> :
       (!isLoading && <Button>Login</Button>)
