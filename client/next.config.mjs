49b86ce0f52596d6344e7b475df0c135ff92c6c0
sbayce <youssef2311@gmail.com>
2024-07-08 05:05:44 +0300

    config for icons

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-}
-
-export default nextConfig
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgo: false, // disable SVGO optimizations if necessary
+            },
+          },
+        ],
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
