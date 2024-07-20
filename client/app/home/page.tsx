eb99335999c1e5f62f83a8d366a003484f0942b4
sbayce <youssef2311@gmail.com>
2024-07-20 21:51:28 +0300

    added react-query

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,16 +1,21 @@
+"use client"
 import React from "react"
 import axios from "axios"
-import { cookies } from "next/headers"
 import Workspace from "../components/home/Workspace"
+import { useEffect, useState } from "react"
+import { useQuery } from "react-query"
 
-const Home = async () => {
+type WorkSpace = {
+  owned: any
+  collaborated: any
+}
+
+const Home = () => {
+  const [workSpace, setWorkSpace] = useState<WorkSpace | null>(null)
+
   const getWorkspace = async () => {
     try {
-      const cookieHeader = cookies().toString() // Extract cookies from incoming request
       const res = await axios.get("http://localhost:4000/user/get-workspace", {
-        headers: {
-          Cookie: cookieHeader, // Pass the cookies in the request headers
-        },
         withCredentials: true,
       })
       return res.data
@@ -18,13 +23,15 @@ const Home = async () => {
       console.log(err)
     }
   }
-  let {owned, collaborated} = await getWorkspace()
-  owned = owned.length === 0? null : owned
-  collaborated = collaborated.length === 0? null : collaborated
+  const {data, isLoading} = useQuery("get-workSpace", getWorkspace)
+  
+  console.log(data)
+  // owned = owned.length === 0? null : owned
+  // collaborated = collaborated.length === 0? null : collaborated
   return (
     <div className="py-8 px-80">
       {/* <h1 className="text-lg font-medium mb-2">Your documents</h1> */}
-      <Workspace owned={owned} collaborated={collaborated} />
+      {data && <Workspace owned={data.owned} collaborated={data.collaborated} />}
     </div>
   )
 }
