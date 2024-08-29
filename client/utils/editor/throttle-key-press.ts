6763ea2cb06892fdaae6cd4d141f1bd7a9a2ca0a
sbayce <youssef2311@gmail.com>
2024-08-29 21:05:15 +0300

    change-emits optimization

--- client/utils/editor/throttle-key-press.ts
+++ client/utils/editor/throttle-key-press.ts
@@ -1,6 +1,7 @@
 import Quill from "quill"
 import { Delta } from "quill/core"
 import { Socket } from "socket.io-client"
+import isEqual from 'lodash.isequal'
 
 const throttle = (mainFunction: Function, delay: number) => {
     let timerFlag: any
@@ -21,8 +22,15 @@ const throttle = (mainFunction: Function, delay: number) => {
           }
           console.log("composed delta: ", parentDelta)
           console.log("old delta: ", oldDelta)
-          socket.emit("send-changes", { delta: parentDelta, oldDelta, index })
-          setAreChangesSent(true)
+          /* emit event if content change
+             current != old --> change
+             current == old --> no-change */
+          const currentContent = JSON.parse(JSON.stringify(quill.getContents()))
+          const oldContent = JSON.parse(JSON.stringify(oldDelta))
+          if(!isEqual(currentContent, oldContent)){ 
+            socket.emit("send-changes", { delta: parentDelta, oldDelta, index })
+            setAreChangesSent(true)
+          }
           // setPreviousDeltas((prevState: Delta[]) => {
           //   const newState = [...prevState]
           //   newState.splice(index, 1)
