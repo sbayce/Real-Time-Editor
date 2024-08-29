6061135bbdf33323e4d34cd10e03def1d9a7180c
sbayce <youssef2311@gmail.com>
2024-08-29 13:00:29 +0300

    .env

--- server/socket/socket.ts
+++ server/socket/socket.ts
@@ -8,7 +8,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
   },
 })
