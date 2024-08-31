897d5a12ece626da88a2e8dfa61f28b932c6fb38
sbayce <youssef2311@gmail.com>
2024-08-31 21:53:14 +0300

    error log

--- server/src/socket/socket.ts
+++ server/src/socket/socket.ts
@@ -186,7 +186,7 @@ io.on("connection", (socket) => {
   })
 })
 
-io.on("connection_error", (err) => {
+io.engine.on("connection_error", (err) => {
   console.log(`connect_error due to ${err.message}`)
 })
 
