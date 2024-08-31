2b5efedb62d18adb0fe464785a5f540518335171
sbayce <youssef2311@gmail.com>
2024-08-31 09:02:05 +0300

    cookies

--- server/src/controller/auth/signup.ts
+++ server/src/controller/auth/signup.ts
@@ -25,15 +25,11 @@ const signup = async (req: Request, res: Response) => {
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
       domain: accessTokenCookieDomain,
-      sameSite: "none",
-      secure: true
     })
     res.cookie("refreshToken", token.refreshToken, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       domain: accessTokenCookieDomain,
-      sameSite: "none",
-      secure: true
     })
     res.status(200).json({ token })
   } catch (error) {
