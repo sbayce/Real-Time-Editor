93b338cc6328307c7232ace3672c4e316181b50e
sbayce <youssef2311@gmail.com>
2024-09-09 23:52:28 +0300

    key handlers impr

--- client/utils/editor/key-down-handlers.ts
+++ client/utils/editor/key-down-handlers.ts
@@ -1,6 +1,7 @@
 import Quill from "quill";
+import { Socket } from "socket.io-client";
 
-const handleKeyDown = (quills: Quill[], q: Quill, index: number, checkPageSize: any) => {
+const handleKeyDown = (quills: Quill[], q: Quill, index: number, exceedsPageSize: any, removeQuill: any, cancelThrottle: any, socket: Socket, parent: any) => {
     return (event: any) => {
         console.log("Key down.")
         const currentSelection = q.getSelection();
@@ -15,10 +16,17 @@ const handleKeyDown = (quills: Quill[], q: Quill, index: number, checkPageSize: 
     
         const moveToNextQuill = () => setTimeout(() => nextQuill?.setSelection(0), 0);
         const moveToPrevQuill = () => setTimeout(() => prevQuill?.setSelection(prevQuill.getLength()), 0);
+        const moveAndDelete = () => setTimeout(() => {
+            q.blur()
+            removeQuill(parent, index)
+            cancelThrottle()
+            socket.emit("remove-page", index)
+            prevQuill.setSelection(prevQuill.getLength())
+        }, 0)
     
         switch (event.key) {
         case "ArrowDown":
-            if (currentLine === lastLine && !checkPageSize(q, index)) moveToNextQuill();
+            if (currentLine === lastLine && !exceedsPageSize(q, index)) moveToNextQuill();
             break;
         case "ArrowRight":
             if (currentLine === lastLine && currentSelection.index === q.getLength() - 1) moveToNextQuill();
@@ -27,11 +35,49 @@ const handleKeyDown = (quills: Quill[], q: Quill, index: number, checkPageSize: 
             if (currentLine === firstLine && currentSelection.index === 0) moveToPrevQuill();
             break;
         case "ArrowUp":
-            if (currentLine === firstLine) moveToPrevQuill();
+            if (currentLine === firstLine && index > 0){
+                    moveToPrevQuill()
+                    // const selection = q.getSelection()
+                    // if (selection) {
+                    //     // Get the bounds of the current cursor position
+                    //     const bounds = q.getBounds(selection.index);
+                    //     console.log("bounds: ", bounds)
+                  
+                    //     if (bounds) {
+                    //       // Get the toolbar element
+                    //     //   const toolbar = document.querySelector('.ql-toolbar') as HTMLElement | null;
+                    //       const toolbar = q.getModule("toolbar") as { container: HTMLElement }
+                  
+                    //       if (toolbar) {
+                    //         const toolbarHeight = toolbar.container.offsetHeight;
+                  
+                    //         // Check if the cursor is above the toolbar
+                    //         const cursorPosition = bounds.top;
+                    //         const editorTop = q.root.getBoundingClientRect().top;
+                  
+                    //         // Check if the cursor is going out of view due to the toolbar overlap
+                    //         console.log(cursorPosition, toolbarHeight)
+                    //         if (cursorPosition < toolbarHeight) {
+                    //           // Scroll to the position where the cursor should be visible, center-aligned
+                    //           console.log("scroll")
+                    //           prevQuill.root.scrollIntoView({ block: 'center', behavior: 'smooth' })
+                    //         //   moveToPrevQuill()
+                    //         }
+                    //       }
+                    //     }
+                    //   }
+                
+            }
             break;
-        // case "Backspace":
-        //     // if (q.getLength() === 1) moveToPrevQuill();
-        //     break;
+        case "Backspace":
+            if(index === 0) break
+            if (q.getLength() === 1) {
+                moveAndDelete()
+            }
+            if(currentLine === firstLine && currentSelection.index === 0 && q.getLength() > 1){
+                moveToPrevQuill()
+            }
+            break;
         }
     }
   }
