8173fd9413f141b46284d2a257207890481855cf
sbayce <youssef2311@gmail.com>
2024-10-03 18:42:57 +0300

    cookie 'sameSite'

--- server/src/controller/auth/logout.ts
+++ server/src/controller/auth/logout.ts
@@ -4,9 +4,11 @@ const logout = async (req: Request, res: Response) => {
     try {
         res.clearCookie("accessToken", {
             httpOnly: true,
+            sameSite: 'none'
         });
         res.clearCookie("refreshToken", {
             httpOnly: true,
+            sameSite: 'none'
         });
       res.status(200).json("Logged out.");
     } catch (err) {
