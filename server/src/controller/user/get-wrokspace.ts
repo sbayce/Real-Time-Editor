943ffa328a119d64d9ad902504903bb04aecab86
youssef khaled <youssef2311@gmail.com>
2024-10-05 23:26:53 +0200

    comment out caching

--- server/src/controller/user/get-wrokspace.ts
+++ server/src/controller/user/get-wrokspace.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from "express"
-import redisClient from "../../redis"
+// import redisClient from "../../redis"
 
 const getWorkspace = async (req: Request, res: Response) => {
   try {
     const { postgres } = req.context
     const userId = req.userId
 
-    const cachedWorkSpace = await redisClient.json.get(`${userId}:workSpace`)
-    if(cachedWorkSpace) {
-      res.status(200).json(cachedWorkSpace)
-      return
-    }
+    // const cachedWorkSpace = await redisClient.json.get(`${userId}:workSpace`)
+    // if(cachedWorkSpace) {
+    //   res.status(200).json(cachedWorkSpace)
+    //   return
+    // }
 
     const {rows} = await postgres.query("SELECT editor_id, isOwner FROM user_access WHERE user_id = $1", [userId])
 
@@ -32,11 +32,11 @@ const getWorkspace = async (req: Request, res: Response) => {
       "SELECT id, title, created_at, updated_at, snap_shot FROM editor WHERE id = ANY($1::int[]) ORDER BY updated_at DESC",
       [collaboratedEditorIds]
     )
-    await redisClient.json.set(`${userId}:workSpace`, '$', {
-      owned: ownedEditors.rows,
-      collaborated: collaboratedEditors.rows
-    })
-    await redisClient.expire(`${userId}:workSpace`, 3600);
+    // await redisClient.json.set(`${userId}:workSpace`, '$', {
+    //   owned: ownedEditors.rows,
+    //   collaborated: collaboratedEditors.rows
+    // })
+    // await redisClient.expire(`${userId}:workSpace`, 3600);
     res.status(200).json({owned: ownedEditors.rows, collaborated: collaboratedEditors.rows})
   } catch (error) {
     console.log(error)
