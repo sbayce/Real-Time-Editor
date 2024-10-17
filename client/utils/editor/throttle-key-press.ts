06afe159ed597b8bfcda809e11845add7255dac8
sbayce <youssef2311@gmail.com>
2024-10-17 22:30:39 +0300

    sending changes from different pages (err fix)

--- client/utils/editor/throttle-key-press.ts
+++ client/utils/editor/throttle-key-press.ts
@@ -8,6 +8,8 @@ const throttle = (delay: number) => {
     let timerFlag: any
     let deltas: Delta[] = []
     let invertedDelta: Delta | null = null
+    let currPageIndex: number | null = null
+    let currOldDelta: Delta | null = null
 
     const setIgnoredDelta = (value: Delta) => {
       if(timerFlag){
@@ -24,12 +26,59 @@ const throttle = (delay: number) => {
         timerFlag = null
         deltas = []
         invertedDelta = null
+        currPageIndex = null
+        currOldDelta = null
+      }
+    }
+
+    const sendChanges = (quill: Quill, oldDelta: Delta, socket: Socket, setAreChangesSent: any, isMaster: boolean, quills: Quill[], setIsChanged: any) => {
+      if(deltas.length !== 0){
+        let parentDelta = deltas[0]
+        for(let i =1; i < deltas.length; i++){
+          parentDelta = parentDelta.compose(deltas[i])
+        }
+        console.log("composed delta: ", parentDelta)
+        /* emit event if content change
+           current != old --> change
+           current == old --> no-change */
+        const oldContent = JSON.parse(JSON.stringify(oldDelta))
+        const currentChanges = JSON.parse(JSON.stringify(oldDelta.compose(parentDelta)))
+
+        if(!isEqual(currentChanges, oldContent)){ 
+          socket.emit("send:changes", { delta: parentDelta, oldDelta: currOldDelta, index: currPageIndex })
+          setAreChangesSent(true)
+          if(isMaster) {
+            console.log("saving to DB...s")
+            const content = getEditorContent(quills)
+            console.log("current content to save: ", content)
+            socket.emit("save:editor", content)
+            setIsChanged(false)
+          }
+        }
+        deltas = []
+        invertedDelta = null
       }
     }
   
     const throttledKeyPress = (delta: Delta, quill: Quill, index: number, socket: Socket, oldDelta: Delta, setAreChangesSent: any, isMaster: boolean, quills: Quill[], setIsChanged: any) => {
       if(!socket) return
       if(timerFlag){
+        if(currPageIndex !== null && currPageIndex !== index){
+          clearTimeout(timerFlag)
+          sendChanges(quill, oldDelta, socket, setAreChangesSent, isMaster, quills, setIsChanged)
+          
+          currPageIndex = index
+          currOldDelta = oldDelta
+          deltas.push(delta)
+          timerFlag = setTimeout(() => { 
+            sendChanges(quill, oldDelta, socket, setAreChangesSent, isMaster, quills, setIsChanged)
+            timerFlag = null
+            currPageIndex = null
+            currOldDelta = null
+          }, delay);
+
+          return
+        }
         if(invertedDelta){
           console.log("--------------IGNORE---------------------")
           console.log("inverted delta: ", invertedDelta)
@@ -43,37 +92,14 @@ const throttle = (delay: number) => {
         }
         return
       }
+      currPageIndex = index
+      currOldDelta = oldDelta
       deltas.push(delta)
       timerFlag = setTimeout(() => { 
-        if(deltas.length !== 0){
-          let parentDelta = deltas[0]
-          for(let i =1; i < deltas.length; i++){
-            parentDelta = parentDelta.compose(deltas[i])
-          }
-          console.log("composed delta: ", parentDelta)
-          /* emit event if content change
-             current != old --> change
-             current == old --> no-change */
-          const currentContent = JSON.parse(JSON.stringify(quill.getContents()))
-          const oldContent = JSON.parse(JSON.stringify(oldDelta))
-          const currentChanges = JSON.parse(JSON.stringify(oldDelta.compose(parentDelta)))
-
-          if(!isEqual(currentChanges, oldContent)){ 
-            socket.emit("send:changes", { delta: parentDelta, oldDelta, index })
-            setAreChangesSent(true)
-            // should refactor
-            if(isMaster) {
-              console.log("saving to DB...s")
-              const content = getEditorContent(quills)
-              console.log("current content to save: ", content)
-              socket.emit("save:editor", content)
-              setIsChanged(false)
-            }
-          }
-          deltas = []
-          invertedDelta = null
-        }
+        sendChanges(quill, oldDelta, socket, setAreChangesSent, isMaster, quills, setIsChanged)
         timerFlag = null
+        currPageIndex = null
+        currOldDelta = null
       }, delay);
     };
     return {
